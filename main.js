127d2e82c7211d08bfe9c2012180c5949b7fcf9d
Spark16x <spark2009971@gmail.com>
2025-06-03 18:54:03 +0530

    update main.js

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 let form=document.querySelector('form'),
 result=document.querySelector('.result');
 
+function back() {
+result.hidden = true;
+form.hidden = false;
+}
+
 form.addEventListener('submit',(ev)=>{
   ev.preventDefault();
   
   let w=document.querySelector('.w').value,
   h=document.querySelector('.h').value,
-  bmi=w/(Math.pow(h/100,2));
+  bmi=w/(Math.pow(h/100,2)),remark,color;
   bmi=bmi.toFixed(2);
-  if (bmi <= 18) {
-    
-  } else {
-    
+  
+  if (bmi <= 18.5) {
+    remark='Your underweigt';
+    color='yellow';
+  } else  if (bmi >= 18.5 && bmi <= 24.9){
+    remark= 'Your normal';
+    color='green';
   }
+  else if (bmi >= 25 && bmi <= 29.9) {
+  remark='Your Overweigt';
+  color='orange';
+}else{
+  remark='Your Obesity';
+  color='red';
+}
+result.innerHTML=`<div class="wegth">Wegth : ${w} kg</div> 
+<div class = "heigth" > Height: ${h} cm</div> 
+<div class = "BMi" > BMI: ${bmi} </div> 
+<div class = "remark" style="color:${color}" > Remark: ${remark} </div>
+  <button onclick="back() " >Back</button>`;
+  result.hidden=false;
+  form.hidden=true;
   console.log(bmi)
   
 })
